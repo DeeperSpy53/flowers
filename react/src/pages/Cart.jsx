b7c362e2578dd197f3a888ae8463bce7469658e6
DeeperSpy <pasha.dolgushev@gmail.com>
2025-02-26 17:27:17 +0500

    fin

--- react/src/pages/Cart.jsx
+++ react/src/pages/Cart.jsx
@@ -5,7 +5,9 @@ import querstionImg from '../assets/img/icons/question.svg'
 
 export default function Cart() {
     const [delivery, setDelivery] = useState('delivery');
-    const [dateInput, setDateInput] = useState('time')
+    const [dateInput, setDateInput] = useState('time');
+    const [recevier, setRecevier] = useState('myself');
+    const [phone, setPhone] = useState('+7(');
 
     const date = new Date();
     const dateNow = `${date.getFullYear()}-${date.getMonth()+1<10?'0'+(date.getMonth()+1):(date.getMonth()+1)}-${date.getDate()}`;
@@ -19,6 +21,22 @@ export default function Cart() {
 
         const res = e.target.value.replace(/(\d+)-(\d+)-(\d+)/,"$3.$2.$1");
         setDateShowState(res)
+    }
+
+    function handleTelChange(e) {
+        console.log(e.target.value);
+        
+
+        switch(phone.length){
+            case 3:{
+
+                break;
+            }
+            case 4:{
+                setPhone(phone + '' + e.target.value[4])
+                break;
+            }
+        }
     }
 
     return (
@@ -101,6 +119,41 @@ export default function Cart() {
                             <input type="checkbox" />   
                             По телефону&nbsp;<span>не говорить</span>&nbsp;что это цветы
                         </label>  
+                    </div>
+                </div>
+
+                <div className={style.delivery__receiver}>
+                    <h3>Получатель</h3>
+
+                    <div className={style.deliveryRec__radio}>
+                        <label className={recevier=='myself'?style.delivery__sel:''}>
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <ellipse cx="9.8374" cy="10" rx="9.8374" ry="10" fill="#E0E0E0" />
+                                <ellipse cx="9.83735" cy="10" rx="6.88618" ry="7" fill="#BDBDBD" />
+                            </svg>
+                            <input type="radio" name='myself' value={dateInput} checked={dateInput == 'myself'} onChange={() => setRecevier('myself')}/>
+                            Я получатель
+                        </label>  
+
+                        <label className={recevier=='someone'?style.delivery__sel:''}>
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <ellipse cx="9.8374" cy="10" rx="9.8374" ry="10" fill="#E0E0E0" />
+                                <ellipse cx="9.83735" cy="10" rx="6.88618" ry="7" fill="#BDBDBD" />
+                            </svg>
+                            <input type="radio" name='someone' value={dateInput} checked={dateInput == 'someone'} onChange={() => setRecevier('someone')}/>
+                            Получатель другой человек
+                        </label>   
+                    </div>
+
+                    <div className={style.deliveryRec__column}>
+                        <label>
+                            Имя и фамилия
+                            <input type="text" placeholder='Анатолий петров'/>
+                        </label>
+                        <label>
+                            Моб. номер
+                            <input type="tel" placeholder='+_(___) ___-__-__' value={phone} autoComplete='tel' onChange={handleTelChange}/>
+                        </label>
                     </div>
                 </div>
             </div>
