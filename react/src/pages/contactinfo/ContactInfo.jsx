fcc02d7c73114a0591f5c8b080100e956ede663a
DeeperSpy <pasha.dolgushev@gmail.com>
2025-03-06 17:19:39 +0500

    added map

--- react/src/pages/contactinfo/ContactInfo.jsx
+++ react/src/pages/contactinfo/ContactInfo.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
 import Path from '../../components/path/Path'
 import style from './contactinfo.module.scss'
+import { YMaps, Map, Placemark } from '@iminside/react-yandex-maps';
+import { IMaskInput } from 'react-imask';
 
 export default function ContactInfo(){
+    const defaultState = {
+        center: [43.115319, 131.897837],
+        zoom: 11,
+    };
+
+    const [name, setName] = useState('')
+    const [mail, setMail] = useState('')
+    const [phone, setPhone] = useState()
+    const [question, setQuestion] = useState()
+
     return(
         <div className={style.contactinfo}>
             <div className={style.contactinfo__wrapper}>
@@ -35,10 +48,28 @@ export default function ContactInfo(){
                             </li>
                         </ul>
 
-                        <div className={style.contactinfo__map}>
-
-                        </div>
+                        <YMaps>
+                            <Map defaultState={defaultState} style={{borderRadius: '10px'}}>
+                                <Placemark width='825px' height='404px' defaultGeometry={[43.115319, 131.897837]}/>
+                            </Map>
+                        </YMaps>
                     </div>
+
+                    <form className={style.contactinfo__form}>
+                        <h2>Остались вопросы? Свяжитесь с нами.</h2>
+
+                        <div className={style.form__block}>
+                            <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder='Имя фамилия'/>
+                            <input type="text" value={mail} onChange={(e) => setMail(e.target.value)} placeholder='Эл. почта'/>
+                            <IMaskInput mask="+7 (000) 000-00-00" placeholder='Моб. номер' onChange={(e) => setPhone(e.target.value)}/>
+                        </div>
+
+                        <div className={style.form__question}>
+                            <textarea placeholder='Возникший вопрос' value={question} onChange={(e) => setQuestion(e.target.value)}></textarea>
+                        </div>
+
+                        <button>Отправить</button>
+                    </form>
                 </div>
             </div>
         </div>
